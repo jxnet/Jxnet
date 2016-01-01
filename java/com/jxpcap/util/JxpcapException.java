0bfe137300d88f3b67c12b9cb69043392cdf4ca9
ardikars <ardikars@yahoo.com>
2016-01-01 19:19:39 +0700

    Updates

--- java/com/jxpcap/util/JxpcapException.java
+++ java/com/jxpcap/util/JxpcapException.java
@@ -4,7 +4,16 @@ public class JxpcapException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+	public JxpcapException() {
+		super();
+	}
 	public JxpcapException(String message) {
 		super(message);
 	}
+	public JxpcapException(String message, Throwable cause) {
+        super(message, cause);
+    }
+	public JxpcapException(Throwable cause) {
+        super(cause);
+    }
 }
