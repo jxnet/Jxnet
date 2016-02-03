9e353cd41db182d6d62c1af8d5b67d40f4d581b4
ardikars <ardikars@yahoo.com>
2016-02-03 14:55:19 +0700

    Fixed warrning (-Wpedantic)

--- c/com_jxpcap_Jxpcap.c
+++ c/com_jxpcap_Jxpcap.c
@@ -4,17 +4,23 @@
 
 JNIEXPORT jobject JNICALL Java_com_jxpcap_Jxpcap_nativeOpenLive
 	(JNIEnv *env, jclass jcls, jstring jdevice_name, jint jsnaplen, jint jpromisc, jint jtimeout, jobject jerrmsg) {
-	char errmsg[256]; errmsg[0] = '\0';
-	const char *device_name = (*env)->GetStringUTFChars(env, jdevice_name, 0);
-	pcap_t *pcap = pcap_open_live(device_name, jsnaplen, jpromisc, jtimeout, errmsg);
+	char errmsg[256];
+	const char *device_name;
+	pcap_t *pcap;
+	jmethodID JxpcapInit;
+	jobject jobj;
+	jfieldID jxpcap_FID;
+	errmsg[0] = '\0';
+	device_name = (*env)->GetStringUTFChars(env, jdevice_name, 0);
+	pcap = pcap_open_live(device_name, jsnaplen, jpromisc, jtimeout, errmsg);
 	setMsg(env, jerrmsg, errmsg);
 	(*env)->ReleaseStringUTFChars(env, jdevice_name, device_name);
 	if(pcap == NULL) {
 		return NULL;
 	}
-	jmethodID JxpcapInit = (*env)->GetMethodID(env, jcls, "<init>", "()V");
-	jobject jobj = (*env)->NewObject(env, jcls, JxpcapInit);
-	jfieldID jxpcap_FID = (*env)->GetFieldID(env, jcls, "jxpcap", "J");
+	JxpcapInit = (*env)->GetMethodID(env, jcls, "<init>", "()V");
+	jobj = (*env)->NewObject(env, jcls, JxpcapInit);
+	jxpcap_FID = (*env)->GetFieldID(env, jcls, "jxpcap", "J");
 	setPcap(env, jobj, jxpcap_FID, pcap);
 	return jobj;
 }
