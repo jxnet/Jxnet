6f3d482d285273e62beb639a486e5b1da7ca800d
ardikars <ardikars@yahoo.com>
2016-02-04 18:36:00 +0700

    Adding default gateway on linux

--- c/util/com_jxpcap_util_Devices.c
+++ c/util/com_jxpcap_util_Devices.c
@@ -21,7 +21,23 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+#include <arpa/inet.h>
 #endif
+
+
+#define BUFSIZE			8192
+
+struct route_info {
+	struct in_addr dst_addr;
+	struct in_addr src_addr;
+	struct in_addr gw_addr;
+	char if_name[16];
+};
+
 
 int compare_strings(char *first, char *second) {
    while (*first == *second) {
@@ -37,6 +53,105 @@ int compare_strings(char *first, char *second) {
    else
       return 0;
 }
+
+
+char *get_gw(struct nlmsghdr *nl_hdr, struct route_info *rt_info, char *if_name) {
+	struct rtmsg *rt_msg;
+	struct rtattr *rt_attr;
+	int rt_len;
+	rt_msg = (struct rtmsg *) NLMSG_DATA(nl_hdr);
+	if ((rt_msg->rtm_family != AF_INET) || (rt_msg->rtm_table != RT_TABLE_MAIN)) {
+		return NULL;
+	}
+	rt_attr = (struct rtattr *) RTM_RTA(rt_msg);
+	rt_len = RTM_PAYLOAD(nl_hdr);
+
+	for (;RTA_OK(rt_attr, rt_len); rt_attr = RTA_NEXT(rt_attr, rt_len)) {
+		switch (rt_attr->rta_type) {
+			case RTA_OIF:
+	            if_indextoname(*(int *) RTA_DATA(rt_attr), rt_info->if_name);
+	            break;
+	        case RTA_GATEWAY:
+	            rt_info->gw_addr.s_addr= *(u_int *) RTA_DATA(rt_attr);
+	            break;
+	        case RTA_PREFSRC:
+	            rt_info->src_addr.s_addr= *(u_int *) RTA_DATA(rt_attr);
+	            break;
+	        case RTA_DST:
+	            rt_info->dst_addr.s_addr= *(u_int *) RTA_DATA(rt_attr);
+	            break;
+		}
+	}
+
+	if(compare_strings(if_name, (char *) rt_info->if_name) == 1) {
+		return (char *) inet_ntoa(rt_info->gw_addr);
+	}
+	return NULL;
+}
+
+
+int read_nl_sock(int sock, char *msg_buf, int msg_seq, int pid) {
+	struct nlmsghdr *nlHdr;
+    int read_len = 0, msg_len = 0;
+    do {
+		if ((read_len = recv(sock, msg_buf, BUFSIZE - msg_len, 0)) < 0) {
+			return -1;
+		}
+		nlHdr = (struct nlmsghdr *) msg_buf;
+		if ((NLMSG_OK(nlHdr, read_len) == 0) || (nlHdr->nlmsg_type == NLMSG_ERROR)) {
+			return -1;
+		}
+		if (nlHdr->nlmsg_type == NLMSG_DONE) {
+			break;
+		} else {
+			msg_buf += read_len;
+			msg_len += read_len;
+		}
+		if ((nlHdr->nlmsg_flags & NLM_F_MULTI) == 0) {
+			break;
+		}
+    } while ((nlHdr->nlmsg_seq != msg_seq) || (nlHdr->nlmsg_pid != pid));
+    return msg_len;
+}
+
+char *get_gateway(char *if_name) {
+	int msg_seq = 0;
+	int sock; int len;
+	char msg_buf[BUFSIZE];
+
+	struct nlmsghdr *nl_msg;
+	//struct rtmsg *rt_msg;
+	struct route_info *rt_info;
+
+	if((sock = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE)) < 0) {
+	    return NULL;
+	}
+	memset(msg_buf, 0, BUFSIZ);
+
+	nl_msg = (struct nlmsghdr *) msg_buf;
+	//rt_msg = (struct rtmsg *) NLMSG_DATA(nl_msg);
+
+	nl_msg->nlmsg_len = NLMSG_LENGTH(sizeof(struct rtmsg));
+	nl_msg->nlmsg_type = RTM_GETROUTE;
+	nl_msg->nlmsg_flags = NLM_F_DUMP | NLM_F_REQUEST;
+	nl_msg->nlmsg_seq = msg_seq++;
+	nl_msg->nlmsg_pid = getpid();
+
+	if(send(sock, nl_msg, nl_msg->nlmsg_len, 0) < 0) {
+	  	return NULL;
+	}
+
+	if ((len = read_nl_sock(sock, msg_buf, msg_seq, getpid())) < 0) {
+	    return NULL;
+	}
+
+	rt_info = (struct route_info *) malloc(sizeof(struct route_info));
+
+	memset(rt_info, 0, sizeof(struct route_info));
+
+	return get_gw(nl_msg, rt_info, if_name);
+}
+
 
 char *get_mac_addr(JNIEnv *env, char *if_name, jobject jerrmsg) {
 	const unsigned char *mac;
@@ -153,7 +268,7 @@ jobject setNetIface(JNIEnv *env, jobject jdevice_list, jmethodID List_addMID, pc
 
 	if(device_list->name != NULL) {
 		jobject jstr = (*env)->NewStringUTF(env, device_list->name);
-		char *ifname = strtok(device_list->name, "_");
+		strtok(device_list->name, "_");
 		char *if_name = strtok(NULL, "_");
 		char *m = get_mac_addr(env, if_name, jerrmsg);
 		if(m != NULL) {
