bee5ea0edaf4ae1c93de2860258cd3ebe739edf1
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-01-28 21:15:56 -0800

    28-01-2017 21:15:56 PST

--- Application.mk
+++ Application.mk
@@ -1,5 +1,5 @@
 #APP_OPTIM := debug 
-APP_ABI := arm64-v8a# x86_64 mips64 armeabi-v7a x86 mips
+APP_ABI := arm64-v8a x86_64 mips64 armeabi-v7a x86 mips
 #APP_ABI := x86 armeabi-v7a mips
 #APP_ABI := arm64-v8a x86_64 mips64	
 APP_PLATFORM := android-19	
