1879950786d586db9c535c4392624b302c4eb99f
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-02-24 08:27:30 +0000

    24-02-2017 08:27:30 UTC

--- jxnet-test/src/test/java/com/ardikars/test/AllTests.java
+++ jxnet-test/src/test/java/com/ardikars/test/AllTests.java
@@ -9,9 +9,8 @@ import org.junit.runners.Suite.SuiteClasses;
 		PcapLookupDev.class, PcapLookupNet.class, PcapLoop.class,
 		PcapNext.class, PcapOpenDead.class, PcapOpenLive.class,
 		PcapOpenOffline.class })
-public class AllTests
-{
-	public static final String deviceName = "eth0";
+public class AllTests {
+	public static final String deviceName = "wlan0";
 	public static final int snaplen = 1500;
 	public static final int promisc = 1;
 	public static final int to_ms = 2000;
