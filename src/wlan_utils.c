e8644edd1052dfb657e3c97a06951f7d7fd2ac69
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-02-24 03:26:54 +0000

    24-02-2017 03:26:54 UTC

--- src/wlan_utils.c
+++ src/wlan_utils.c
@@ -1,11 +1,20 @@
-
-/*
- * Author	: Ardika Rommy Sanjaya
- * Website	: http://ardikars.com
- * Contact	: contact@ardikars.com
- * License	: Lesser GNU Public License Version 3
+/**
+ * Copyright (C) 2017  Ardika Rommy Sanjaya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include "../src/ids.h"
 #include "../include/jxnet/com_ardikars_jxnet_util_WLan_WLanOperationMode.h"
 #include "../include/jxnet/com_ardikars_jxnet_util_WLan.h"
@@ -20,7 +29,7 @@
 #include <tchar.h>
 #include <conio.h>
 #else
-// 
+
 #endif
 
 #ifdef UNICODE
