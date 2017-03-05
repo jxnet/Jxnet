0be9137ef8130ff5fe70a0ee1c93d1c66f140eac
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-03-05 12:15:58 +0000

    05-03-2017 12:15:58 UTC

--- jxnet-core/src/test/java/com/ardikars/test/AllTests.java
+++ jxnet-core/src/test/java/com/ardikars/test/AllTests.java
@@ -6,10 +6,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({  PcapDumpOpen.class, PcapFindAllDevs.class,
+@SuiteClasses({ PcapDumpOpen.class, PcapFindAllDevs.class,
 		PcapLookupDev.class, PcapLookupNet.class, PcapLoop.class,
 		PcapNext.class, PcapOpenDead.class, PcapOpenLive.class,
-		PcapOpenOffline.class })
+		PcapOpenOffline.class, PcapBreakLoop.class,
+		PcapDatalink.class, PcapDispatch.class, Preconditions.class })
 public class AllTests {
 	private static StringBuilder errbuf = new StringBuilder();
 	public static final String deviceName = AddrUtils.LookupDev(errbuf);
