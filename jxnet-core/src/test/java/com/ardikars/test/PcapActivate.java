19eb044273ce1af85707737ff2039699b0b78730
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-03-24 10:13:11 +0000

    24-03-2017 10:13:11 UTC

---
+++ jxnet-core/src/test/java/com/ardikars/test/PcapActivate.java
@@ -0,0 +1,40 @@
+package com.ardikars.test;
+
+import com.ardikars.jxnet.Jxnet;
+import com.ardikars.jxnet.Pcap;
+import com.ardikars.jxnet.PcapPktHdr;
+import org.junit.*;
+
+import java.nio.ByteBuffer;
+
+public class PcapActivate {
+
+    @org.junit.Test
+    public void run() {
+        StringBuilder errbuf = new StringBuilder();
+        Pcap pcap = Jxnet.PcapCreate(AllTests.deviceName, errbuf);
+        Jxnet.PcapSetSnaplen(pcap, 1500);
+        Jxnet.PcapSetPromisc(pcap, 1);
+        Jxnet.PcapSetTimeout(pcap, 1000);
+        Jxnet.PcapSetImmediateMode(pcap, 1);
+        if (Jxnet.PcapCanSetRfMon(pcap) == 1) {
+            Jxnet.PcapSetRfMon(pcap);
+        }
+        Jxnet.PcapActivate(pcap);
+
+        int i=0;
+        ByteBuffer buffer;
+        PcapPktHdr pktHdr = new PcapPktHdr();
+        while (i < 10) {
+            buffer = Jxnet.PcapNext(pcap, pktHdr);
+            if (buffer == null) {
+                System.out.println("Timeout.");
+            } else {
+                System.out.println(pktHdr);
+            }
+            i++;
+        }
+        Jxnet.PcapClose(pcap);
+    }
+
+}
