1cf2dff2607664b901f2f221dde42baaabfeacc3
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-04-09 18:53:25 -0700

    09-04-2017 18:53:25 PDT

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,19 +10,18 @@ LOCAL_SRC_FILES := \
 	src/ids.c \
 	src/utils.c \
 	src/addr_utils.c \
-	src/preconditions.c \
-	src/arp.c
+	src/preconditions.c
 
 LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/libdnet/src \
-	$(LOCAL_PATH)/libdnet/include \
 	$(LOCAL_PATH)/libpcap
 
-LOCAL_STATIC_LIBRARIES := libdnet libpcap
+LOCAL_STATIC_LIBRARIES := libpcap
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(LOCAL_PATH)/libdnet/Android.mk 
-
-include $(LOCAL_PATH)/../libpcap/Android.mk 
+ifeq ($(TARGET_ARCH_ABI), arm64-v8a)	
+	include $(LOCAL_PATH)/libpcap/Android.mk 
+else
+	include $(LOCAL_PATH)/libpcap/Android.mk 
+endif
 
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,6 @@
 #APP_OPTIM := debug 
-APP_ABI := arm64-v8a x86_64 mips64 armeabi-v7a x86 mips
+APP_ABI := armeabi-v7a x86 mips arm64-v8a x86_64 mips64 
+#APP_STL := stlport_static
 #APP_ABI := x86 armeabi-v7a mips
 #APP_ABI := arm64-v8a x86_64 mips64	
-APP_PLATFORM := android-19	
+APP_PLATFORM := android-19
