a92d5fd8b504a46b4d9378d09495b60ff4d9eca3
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-04-15 21:00:41 -0700

    15-04-2017 21:00:25 PDT

--- jni/scripts/WinBuildShared.mk
+++ jni/scripts/WinBuildShared.mk
@@ -1,5 +1,4 @@
 
-
 ifneq ($(OS), Windows_NT)
 	$(error This build script just for Windows NT.)
 endif
@@ -8,41 +7,35 @@ ifeq ($(JAVA_HOME), )
 	$(error JAVA_HOME environment is not set.)
 endif
 
+CC = gcc
+CFLAGS = -Wall 
+LDFLAGS = -Wl,--export-all-symbols -Wl,--add-stdcall-alias -shared
+
+CFLAGS += -I$(JAVA_HOME)/include -I$(JAVA_HOME)/include/win32 -I../include 
+
 ifeq ($(PROCESSOR_ARCHITECTURE), AMD64)
-	ARCH=x64
-	CFLAGS=-Wl,--export-all-symbols -Wl,--add-stdcall-alias -m64 -shared
+	LDFLAGS += -m64 -L../lib/x64
 endif
 
 ifeq ($(PROCESSOR_ARCHITECTURE), x86)
-	ARCH=
-	CFLAGS=-Wl,--export-all-symbols -Wl,--add-stdcall-alias -m32 -shared
-endif
-
-ifeq ($(WpdPack), )
-	$(error can not find Winpcap SDK)
+	LDFLAGS += -m64 -L../lib/x64
 endif
 
 SRC = \
+	../src/ids.c \
+	../src/preconditions.c \
+	../src/bpf.c \
 	../src/jxnet.c \
 	../src/utils.c \
-	../src//addr_utils.c \
-	../src/ids.c \
-	../src/bpf.c	
-
-
-CC = gcc
-
-CFLAGS += -I"$(JAVA_HOME)/include" -I"$(JAVA_HOME)/include/win32" -I"$(WpdPack)/Include" -L"$(WpdPack)/Lib/$(ARCH)" -Wall -Werror
-
-LDFLAGS = -lwpcap -liphlpapi
+	../src/addr_utils.c \
+	../src/mac_address.c
 
 LIB_NAME = jxnet
 
 all: $(LIB_NAME)
 
 $(LIB_NAME):
-	$(CC) $(CFLAGS) $(SRC) $(LDFLAGS) -o $@.dll
+	$(CC) $(CFLAGS) $(LDFLAGS) $(SRC) -o $@.dll -lwpcap -liphlpapi
 	
-
 clean:
 	rm $(LIB_NAME).*
