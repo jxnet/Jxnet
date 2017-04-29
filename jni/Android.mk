3f339f6d95716bb88f927886a403cc70e12905dc
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-04-29 10:00:03 -0700

    29-04-2017 10:00:03 PDT

--- jni/Android.mk
+++ jni/Android.mk
@@ -21,22 +21,3 @@ LOCAL_STATIC_LIBRARIES := libpcap
 include $(BUILD_SHARED_LIBRARY)
 
 include $(LOCAL_PATH)/libpcap/Android.mk 
-
-
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := setcap
-
-LOCAL_SRC_FILES := \
-	src/setcap.c
-	
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/libcap/include \
-	$(LOCAL_PATH)/libcap/include/uapi
-        
-LOCAL_STATIC_LIBRARIES := libcap
-	
-include $(BUILD_EXECUTABLE)
-
-include $(LOCAL_PATH)/libcap/Android.mk 
