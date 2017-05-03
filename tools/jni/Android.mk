cba4072de159f8136ff97649988e50480e46d1ff
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-05-02 17:10:10 -0700

    02-05-2017 17:10:10 PDT

--- tools/jni/Android.mk
+++ tools/jni/Android.mk
@@ -1,22 +1,4 @@
+CURRENT_DIR := $(call my-dir)
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := setcap
-
-LOCAL_SRC_FILES:= \
-        progs/setcap.c
-
-LOCAL_C_INCLUDES := \
-        $(LOCAL_PATH)/libcap/include \
-        $(LOCAL_PATH)/libcap/include/uapi
-
-LOCAL_CFLAGS := -fPIC -Wall
-LOCAL_CFLAGS += -Wall -Wwrite-strings -Wpointer-arith -Wcast-qual -Wcast-align -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wshadow
-
-LOCAL_STATIC_LIBRARIES := libcap
-
-include $(BUILD_EXECUTABLE)
-
-include $(LOCAL_PATH)/libcap/Android.mk 
+include $(CURRENT_DIR)/libcap/Android.mk
+include $(CURRENT_DIR)/progs/Android.mk
---
+++ tools/jni/progs/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := setcap
+LOCAL_SRC_FILES := setcap.c
+LOCAL_C_INCLUDES := \
+	${LOCAL_PATH}/../libcap/include \
+	$(LOCAL_PATH)/../libcap/include/uapi
+LOCAL_CFLAGS := Wall -Wwrite-strings -Wpointer-arith -Wcast-qual -Wcast-align -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wshadow
+LOCAL_STATIC_LIBRARIES := cap
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := getcap
+LOCAL_SRC_FILES := getcap.c
+LOCAL_C_INCLUDES := \
+	${LOCAL_PATH}/../libcap/include \
+	$(LOCAL_PATH)/../libcap/include/uapi
+LOCAL_CFLAGS := Wall -Wwrite-strings -Wpointer-arith -Wcast-qual -Wcast-align -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wshadow
+LOCAL_STATIC_LIBRARIES := cap
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := getpcaps
+LOCAL_SRC_FILES := getpcaps.c
+LOCAL_C_INCLUDES := \
+	${LOCAL_PATH}/../libcap/include \
+	$(LOCAL_PATH)/../libcap/include/uapi
+LOCAL_CFLAGS := Wall -Wwrite-strings -Wpointer-arith -Wcast-qual -Wcast-align -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wshadow
+LOCAL_STATIC_LIBRARIES := cap
+include $(BUILD_EXECUTABLE)
