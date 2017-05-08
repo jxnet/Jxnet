484250380446ea97f7b84fc88b91ed9245917acc
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-05-08 02:37:23 -0700

    08-05-2017 02:37:22 PDT

--- jni/Android.mk
+++ jni/Android.mk
@@ -13,11 +13,47 @@ LOCAL_SRC_FILES := \
 	src/preconditions.c \
 	src/mac_address.c
 
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/libpcap
-
 LOCAL_STATIC_LIBRARIES := libpcap
 
-include $(BUILD_SHARED_LIBRARY)
+ifeq ($(TARGET_ARCH_ABI), armeabi)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap/Android.mk
+endif
 
-include $(LOCAL_PATH)/libpcap/Android.mk 
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap/Android.mk
+endif
+
+ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap-latest
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap-latest/Android.mk
+endif
+
+ifeq ($(TARGET_ARCH_ABI), x86)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap/Android.mk
+endif
+
+ifeq ($(TARGET_ARCH_ABI), x86_64)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap-latest
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap-latest/Android.mk
+endif
+
+ifeq ($(TARGET_ARCH_ABI), mips)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap/Android.mk
+endif
+
+ifeq ($(TARGET_ARCH_ABI), mips64)
+	LOCAL_C_INCLUDES := $(LOCAL_PATH)/libpcap-latest
+	include $(BUILD_SHARED_LIBRARY)
+	include $(LOCAL_PATH)/libpcap-latest/Android.mk
+endif
+
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,7 @@
 NDK_TOOLCHAIN_VERSION := 4.9
-#APP_OPTIM := debug 
-APP_ABI := armeabi-v7a x86 mips arm64-v8a x86_64 mips64 
+#APP_OPTIM := debug
+APP_ABI := armeabi armeabi-v7a x86 mips arm64-v8a x86_64 mips64 
 #APP_STL := stlport_static
-#APP_ABI := x86 armeabi-v7a mips
-#APP_ABI := arm64-v8a x86_64 mips64	
+#APP_ABI := x86 armeabi armeabi-v7a mips
+#APP_ABI := arm64-v8a x86_64 mips64
 APP_PLATFORM := android-19
