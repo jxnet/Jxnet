a1187b1f9aa799218f66594eb3522b527f52ac55
Ardika Rommy Sanjaya <contact@ardikars.com>
2017-05-09 23:11:44 -0700

    09-05-2017 23:11:43 PDT

--- jni/Application.mk
+++ jni/Application.mk
@@ -4,4 +4,4 @@ APP_ABI := armeabi armeabi-v7a x86 mips arm64-v8a x86_64 mips64
 #APP_STL := stlport_static
 #APP_ABI := x86 armeabi armeabi-v7a mips
 #APP_ABI := arm64-v8a x86_64 mips64
-APP_PLATFORM := android-19
+APP_PLATFORM := android-21
