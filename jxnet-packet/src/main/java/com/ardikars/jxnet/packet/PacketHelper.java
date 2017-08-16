207ec4a65f1e25ad5e72f794afed1e06c5fcaa53
Ardika Rommy Sanjaya <ardikars@gmail.com>
2017-08-16 11:59:56 +0700

    16-08-2017 11:59:56 WIB

--- jxnet-packet/src/main/java/com/ardikars/jxnet/packet/PacketHelper.java
+++ jxnet-packet/src/main/java/com/ardikars/jxnet/packet/PacketHelper.java
@@ -17,163 +17,153 @@
 
 package com.ardikars.jxnet.packet;
 
-import com.ardikars.jxnet.*;
+import com.ardikars.jxnet.DataLinkType;
+import com.ardikars.jxnet.Pcap;
+import com.ardikars.jxnet.PcapHandler;
+import com.ardikars.jxnet.PcapPktHdr;
+import com.ardikars.jxnet.annotation.Type;
 import com.ardikars.jxnet.packet.ethernet.Ethernet;
 import com.ardikars.jxnet.packet.radiotap.RadioTap;
 import com.ardikars.jxnet.packet.sll.SLL;
 import com.ardikars.jxnet.util.ByteUtils;
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executor;
+
+import static com.ardikars.jxnet.Jxnet.*;
 
 /**
  * @author Ardika Rommy Sanjaya
  * @since 1.1.0
  */
+@Deprecated
 public class PacketHelper {
 
-    public static <T> int loop(Pcap pcap, int count, PacketListener<T> packetListener, T arg) {
-        PcapHandler<PacketListener<T>> handler = (user, h, bytes) -> {
-            List<Packet> packets = parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
-            user.nextPacket(arg, h, packets);
+    public static <T> int loop(Pcap pcap, int count, PacketHandler<T> handler, T arg) {
+        DataLinkType datalinkType = pcap.getDataLinkType();
+        PcapHandler<PacketHandler<T>> callback = (tPacketHandler, pcapPktHdr, buffer) -> {
+            if (pcapPktHdr == null || buffer == null) return;
+            tPacketHandler.nextPacket(arg, pcapPktHdr, parsePacket(datalinkType, ByteUtils.toByteArray(buffer)));
         };
-        return Jxnet.PcapLoop(pcap, count, handler, packetListener);
+        return PcapLoop(pcap, count, callback, handler);
     }
 
-    public static <T> int loop(Pcap pcap, int count, PacketHandler<T> packetHandler, T arg) {
-        PcapHandler<PacketHandler<T>> handler = (user, h, bytes) -> {
-            Map<Class, Packet> packets = parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
-            user.nextPacket(arg, h, packets);
-        };
-        return Jxnet.PcapLoop(pcap, count, handler, packetHandler);
+    public static Map<Class, Packet> next(Pcap pcap, PcapPktHdr pcapPktHdr) {
+        DataLinkType datalinkType = pcap.getDataLinkType();
+        ByteBuffer buffer = PcapNext(pcap, pcapPktHdr);
+        if (buffer == null) return null;
+        return parsePacket(datalinkType, ByteUtils.toByteArray(buffer));
     }
 
-    public static <T, V extends Packet> int loop(Pcap pcap, int count, PacketProcessor<T, V> handler, T arg) {
-        PcapHandler<PacketProcessor<T, V>> callback = (user, h, bytes) -> {
-            user.initialize(arg, pcap, h);
-            user.decode(ByteUtils.toByteArray(bytes));
-        };
-        return Jxnet.PcapLoop(pcap, count, callback, handler);
-    }
-
-    public static List<Packet> nextList(Pcap pcap, PcapPktHdr pcapPktHdr) {
-        ByteBuffer buffer = Jxnet.PcapNext(pcap, pcapPktHdr);
-        return parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
-    }
-
-    public static Map<Class, Packet> nextMap(Pcap pcap, PcapPktHdr pcapPktHdr) {
-        ByteBuffer buffer = Jxnet.PcapNext(pcap, pcapPktHdr);
-        return parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
-    }
-
-    public static int nextExList(Pcap pcap, PcapPktHdr pktHdr, List<Packet> packets) {
+    public static int nextEx(Pcap pcap, PcapPktHdr pktHdr, HashMap<Class, Packet> packets) {
         packets.clear();
+        DataLinkType datalinkType = pcap.getDataLinkType();
         ByteBuffer buffer = ByteBuffer.allocateDirect(pcap.getSnapshotLength());
-        int ret = Jxnet.PcapNextEx(pcap, pktHdr, buffer);
-        List<Packet> pkts = parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
-        packets.addAll(pkts);
-        return ret;
-    }
-
-    public static int nextExMap(Pcap pcap, PcapPktHdr pktHdr, HashMap<Class, Packet> packets) {
-        packets.clear();
-        ByteBuffer buffer = ByteBuffer.allocateDirect(pcap.getSnapshotLength());
-        int ret = Jxnet.PcapNextEx(pcap, pktHdr, buffer);
-        Map<Class, Packet> pkts = parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
+        int ret = PcapNextEx(pcap, pktHdr, buffer);
+        if (pktHdr == null || buffer == null) return -1;
+        Map<Class, Packet> pkts = parsePacket(datalinkType, ByteUtils.toByteArray(buffer));
         packets.putAll(pkts);
         return ret;
     }
 
-    public static Map<Class, Packet> parseMap(DataLinkType linkType, byte[] bytes) {
-        Map<Class, Packet> packets = new HashMap<Class, Packet>();
-        if (bytes == null) {
-            packets.clear();
-            return packets;
-        }
+    private static Packet parsePacket(DataLinkType dataLinkType, byte[] bytes, Type type) {
         Packet packet = null;
-        switch (linkType) {
+        Packet result = null;
+        int packetNumber = 1;
+        int typeNumber = 0;
+        if (type != null) {
+            typeNumber = type.number();
+        }
+        switch (dataLinkType) {
             case EN10MB:
                 packet = Ethernet.newInstance(bytes);
-                packets.put(packet.getClass(), packet);
+                if (type == null) {
+                    return packet;
+                }
+                while (packet != null) {
+                    if (packet.getClass() == type.value()) {
+                        if (typeNumber == 0) {
+                            result = packet;
+                        } else {
+                            if (packetNumber == typeNumber) {
+                                return packet;
+                            } else {
+                                packetNumber = packetNumber + 1;
+                            }
+                        }
+                    }
+                    packet = packet.getPacket();
+                }
+                return result;
+            case IEEE802_11_RADIO:
+                packet = RadioTap.newInstance(bytes);
+                if (type == null) {
+                    return packet;
+                }
+                while (packet != null) {
+                    if (packet.getClass() == type.value()) {
+                        if (typeNumber == 0) {
+                            result = packet;
+                        } else {
+                            if (packetNumber == typeNumber) {
+                                return packet;
+                            } else {
+                                packetNumber = packetNumber + 1;
+                            }
+                        }
+                    }
+                    packet = packet.getPacket();
+                }
+                return result;
+            case LINUX_SLL:
+                packet = SLL.newInstance(bytes);
+                if (type == null) {
+                    return packet;
+                }
+                while (packet != null) {
+                    if (packet.getClass() == type.value()) {
+                        if (typeNumber == 0) {
+                            result = packet;
+                        } else {
+                            if (packetNumber == typeNumber) {
+                                return packet;
+                            } else {
+                                packetNumber = packetNumber + 1;
+                            }
+                        }
+                    }
+                    packet = packet.getPacket();
+                }
+                return result;
+            default:
+                return UnknownPacket.newInstance(bytes);
+        }
+    }
+
+    private static Map<Class, Packet> parsePacket(DataLinkType datalinkType, byte[] bytes) {
+        Map<Class, Packet> pkts = new HashMap<Class, Packet>();
+        Packet packet = null;
+        switch (datalinkType) {
+            case EN10MB:
+                packet = Ethernet.newInstance(bytes);
+                pkts.put(packet.getClass(), packet);
                 break;
             case IEEE802_11_RADIO:
                 packet = RadioTap.newInstance(bytes);
-                packets.put(packet.getClass(), packet);
+                pkts.put(packet.getClass(), packet);
                 break;
             case LINUX_SLL:
                 packet = SLL.newInstance(bytes);
-                packets.put(packet.getClass(), packet);
+                pkts.put(packet.getClass(), packet);
                 break;
             default:
                 packet = UnknownPacket.newInstance(bytes);
         }
         while ((packet = packet.getPacket()) != null) {
-            packets.put(packet.getClass(), packet);
+            pkts.put(packet.getClass(), packet);
         }
-        return packets;
-    }
-
-    public static List<Packet> parseList(DataLinkType linkType, byte[] bytes) {
-        List<Packet> packets = new ArrayList<>();
-        if (bytes == null) {
-            packets.clear();
-            return packets;
-        }
-        Packet packet = null;
-        switch (linkType) {
-            case EN10MB:
-                packet = Ethernet.newInstance(bytes);
-                packets.add(packet);
-                break;
-            case IEEE802_11_RADIO:
-                packet = RadioTap.newInstance(bytes);
-                packets.add(packet);
-                break;
-            case LINUX_SLL:
-                packet = SLL.newInstance(bytes);
-                packets.add(packet);
-                break;
-            default:
-                packet = UnknownPacket.newInstance(bytes);
-        }
-        while ((packet = packet.getPacket()) != null) {
-            packets.add(packet);
-        }
-        return packets;
-    }
-
-    public static <T> int loop(Pcap pcap, int count, PacketListener<T> packetListener, T arg, Executor executor) {
-        PcapHandler<PacketListener> handler = (user, h, bytes) -> {
-            executor.execute(() -> {
-                List<Packet> packets = parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
-                user.nextPacket(arg, h, packets);
-            });
-        };
-        return Jxnet.PcapLoop(pcap, count, handler, packetListener);
-    }
-
-    public static <T> int loop(Pcap pcap, int count, PacketHandler<T> packetHandler, T arg, Executor executor) {
-        PcapHandler<PacketHandler> handler = (user, h, bytes) -> {
-            executor.execute(() -> {
-                Map<Class, Packet> packets = parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
-                user.nextPacket(arg, h, packets);
-            });
-        };
-        return Jxnet.PcapLoop(pcap, count, handler, packetHandler);
-    }
-
-    public static <T, V extends Packet> int loop(Pcap pcap, int count, PacketProcessor<T, V> handler, T arg, Executor executor) {
-        PcapHandler<PacketProcessor<T, V>> callback = (user, h, bytes) -> {
-            executor.execute(() -> {
-                user.initialize(arg, pcap, h);
-                user.decode(ByteUtils.toByteArray(bytes));
-            });
-        };
-        return Jxnet.PcapLoop(pcap, count, callback, handler);
+        return pkts;
     }
 
 }
--- jxnet-packet/src/main/java/com/ardikars/jxnet/packet/PacketListener.java
+++ jxnet-packet/src/main/java/com/ardikars/jxnet/packet/PacketListener.java
@@ -1,39 +1,188 @@
-/**
- * Copyright (C) 2017  Ardika Rommy Sanjaya
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.ardikars.jxnet.packet;
 
-import com.ardikars.jxnet.PcapPktHdr;
+import com.ardikars.jxnet.*;
+import com.ardikars.jxnet.packet.ethernet.Ethernet;
+import com.ardikars.jxnet.packet.radiotap.RadioTap;
+import com.ardikars.jxnet.packet.sll.SLL;
+import com.ardikars.jxnet.util.ByteUtils;
 
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.Executor;
 
 /**
- * @author Ardika Rommy Sanjaya
- * @since 1.1.0
+ * Created by Ardika on 16/08/17.
+ * All will be fine
  */
-@FunctionalInterface
-public interface PacketListener<T> {
+public class PacketListener {
 
-    /**
-     * Next available packet.
-     * @param arg user argument.
-     * @param pktHdr PcapPktHdr.
-     * @param packet packet.
-     */
-    void nextPacket(T arg, PcapPktHdr pktHdr, List<Packet> packet);
+    @FunctionalInterface
+    public interface Map<T> {
+
+        /**
+         * Next available packet.
+         * @param arg user argument.
+         * @param pktHdr PcapPktHdr.
+         * @param packets packet.
+         */
+        void nextPacket(T arg, PcapPktHdr pktHdr, java.util.Map<Class, Packet> packets);
+
+    }
+
+    @FunctionalInterface
+    public interface List<T> {
+
+        /**
+         * Next available packet.
+         * @param arg user argument.
+         * @param pktHdr PcapPktHdr.
+         * @param packets packet.
+         */
+        void nextPacket(T arg, PcapPktHdr pktHdr, java.util.List<Packet> packets);
+
+    }
+
+    public static <T> int loop(Pcap pcap, int count, PacketListener.List<T> packetList, T arg) {
+        PcapHandler<PacketListener.List<T>> handler = (user, h, bytes) -> {
+            java.util.List<Packet> packets = parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
+            user.nextPacket(arg, h, packets);
+        };
+        return Jxnet.PcapLoop(pcap, count, handler, packetList);
+    }
+
+    public static <T> int loop(Pcap pcap, int count, PacketListener.Map<T> packetMap, T arg) {
+        PcapHandler<PacketListener.Map<T>> handler = (user, h, bytes) -> {
+            java.util.Map<Class, Packet> packets = parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
+            user.nextPacket(arg, h, packets);
+        };
+        return Jxnet.PcapLoop(pcap, count, handler, packetMap);
+    }
+
+    public static <T, V extends Packet> int loop(Pcap pcap, int count, PacketProcessor<T, V> handler, T arg) {
+        PcapHandler<PacketProcessor<T, V>> callback = (user, h, bytes) -> {
+            user.initialize(arg, pcap, h);
+            user.decode(ByteUtils.toByteArray(bytes));
+        };
+        return Jxnet.PcapLoop(pcap, count, callback, handler);
+    }
+
+    public static java.util.List<Packet> nextList(Pcap pcap, PcapPktHdr pcapPktHdr) {
+        ByteBuffer buffer = Jxnet.PcapNext(pcap, pcapPktHdr);
+        return parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
+    }
+
+    public static java.util.Map<Class, Packet> nextMap(Pcap pcap, PcapPktHdr pcapPktHdr) {
+        ByteBuffer buffer = Jxnet.PcapNext(pcap, pcapPktHdr);
+        return parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
+    }
+
+    public static int nextExList(Pcap pcap, PcapPktHdr pktHdr, java.util.List<Packet> packets) {
+        packets.clear();
+        ByteBuffer buffer = ByteBuffer.allocateDirect(pcap.getSnapshotLength());
+        int ret = Jxnet.PcapNextEx(pcap, pktHdr, buffer);
+        java.util.List<Packet> pkts = parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
+        packets.addAll(pkts);
+        return ret;
+    }
+
+    public static int nextExMap(Pcap pcap, PcapPktHdr pktHdr, HashMap<Class, Packet> packets) {
+        packets.clear();
+        ByteBuffer buffer = ByteBuffer.allocateDirect(pcap.getSnapshotLength());
+        int ret = Jxnet.PcapNextEx(pcap, pktHdr, buffer);
+        java.util.Map<Class, Packet> pkts = parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(buffer));
+        packets.putAll(pkts);
+        return ret;
+    }
+
+    public static java.util.Map<Class, Packet> parseMap(DataLinkType linkType, byte[] bytes) {
+        java.util.Map<Class, Packet> packets = new HashMap<Class, Packet>();
+        if (bytes == null) {
+            packets.clear();
+            return packets;
+        }
+        Packet packet = null;
+        switch (linkType) {
+            case EN10MB:
+                packet = Ethernet.newInstance(bytes);
+                packets.put(packet.getClass(), packet);
+                break;
+            case IEEE802_11_RADIO:
+                packet = RadioTap.newInstance(bytes);
+                packets.put(packet.getClass(), packet);
+                break;
+            case LINUX_SLL:
+                packet = SLL.newInstance(bytes);
+                packets.put(packet.getClass(), packet);
+                break;
+            default:
+                packet = UnknownPacket.newInstance(bytes);
+        }
+        while ((packet = packet.getPacket()) != null) {
+            packets.put(packet.getClass(), packet);
+        }
+        return packets;
+    }
+
+    public static java.util.List<Packet> parseList(DataLinkType linkType, byte[] bytes) {
+        java.util.List<Packet> packets = new ArrayList<>();
+        if (bytes == null) {
+            packets.clear();
+            return packets;
+        }
+        Packet packet = null;
+        switch (linkType) {
+            case EN10MB:
+                packet = Ethernet.newInstance(bytes);
+                packets.add(packet);
+                break;
+            case IEEE802_11_RADIO:
+                packet = RadioTap.newInstance(bytes);
+                packets.add(packet);
+                break;
+            case LINUX_SLL:
+                packet = SLL.newInstance(bytes);
+                packets.add(packet);
+                break;
+            default:
+                packet = UnknownPacket.newInstance(bytes);
+        }
+        while ((packet = packet.getPacket()) != null) {
+            packets.add(packet);
+        }
+        return packets;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> int loop(Pcap pcap, int count, PacketListener.List<T> packetList, T arg, Executor executor) {
+        PcapHandler<PacketListener.List<T>> handler = (user, h, bytes) -> {
+            executor.execute(() -> {
+                java.util.List<Packet> packets = parseList(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
+                user.nextPacket(arg, h, packets);
+            });
+        };
+        return Jxnet.PcapLoop(pcap, count, handler, packetList);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> int loop(Pcap pcap, int count, PacketListener.Map<T> packetMap, T arg, Executor executor) {
+        PcapHandler<PacketListener.Map<T>> handler = (user, h, bytes) -> {
+            executor.execute(() -> {
+                java.util.Map<Class, Packet> packets = parseMap(pcap.getDataLinkType(), ByteUtils.toByteArray(bytes));
+                user.nextPacket(arg, h, packets);
+            });
+        };
+        return Jxnet.PcapLoop(pcap, count, handler, packetMap);
+    }
+
+    public static <T, V extends Packet> int loop(Pcap pcap, int count, PacketProcessor<T, V> handler, T arg, Executor executor) {
+        PcapHandler<PacketProcessor<T, V>> callback = (user, h, bytes) -> {
+            executor.execute(() -> {
+                user.initialize(arg, pcap, h);
+                user.decode(ByteUtils.toByteArray(bytes));
+            });
+        };
+        return Jxnet.PcapLoop(pcap, count, callback, handler);
+    }
 
 }
