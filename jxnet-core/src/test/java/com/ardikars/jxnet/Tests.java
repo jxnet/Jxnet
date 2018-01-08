96311d1deb37d2b7b205fee5cfbcb10d2822d51d
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-01-08 17:07:06 +0700

    08-01-2018 17:07:06 WIB

---
+++ jxnet-core/src/test/java/com/ardikars/jxnet/Tests.java
@@ -0,0 +1,16 @@
+package com.ardikars.jxnet;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class Tests {
+
+    @Test
+    public void pcapLibVersion() {
+        Assert.assertNotEquals(Jxnet.PcapLibVersion(), null);
+    }
+
+}
