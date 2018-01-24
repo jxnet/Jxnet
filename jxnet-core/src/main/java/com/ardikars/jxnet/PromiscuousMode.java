9cb75301752d195608796b20ee134c6fea04070c
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-01-24 05:16:35 -0800

    Update license header

--- jxnet-core/src/main/java/com/ardikars/jxnet/PromiscuousMode.java
+++ jxnet-core/src/main/java/com/ardikars/jxnet/PromiscuousMode.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2017  Ardika Rommy Sanjaya
+ * Copyright (C) 2017-2018  Ardika Rommy Sanjaya <contact@ardikars.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
