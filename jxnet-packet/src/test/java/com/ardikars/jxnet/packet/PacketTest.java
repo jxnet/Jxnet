b809e6022af9a617273c45614f7a90317fe452db
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-09-11 11:00:51 +0700

    Fix test

--- jxnet-packet/src/test/java/com/ardikars/jxnet/packet/PacketTest.java
+++ jxnet-packet/src/test/java/com/ardikars/jxnet/packet/PacketTest.java
@@ -1,7 +1,6 @@
 package com.ardikars.jxnet.packet;
 
-import com.ardikars.common.net.MacAddress;
-import jnr.ffi.*;
+import jnr.ffi.LibraryLoader;
 import jnr.ffi.Runtime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,27 +11,6 @@ public class PacketTest {
 
     NativeMappings mappings = LibraryLoader.create(NativeMappings.class).load("Packet");
     Runtime runtime = Runtime.getRuntime(mappings);
-
-    public static void printMacAddress(Struct.Unsigned8[] mac) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < mac.length; i++) {
-            sb.append(String.format("%02X%s", mac[i].get(), (i < mac.length - 1) ? "-" : ""));
-        }
-        System.out.println(sb.toString());
-    }
-
-    @Test
-    public void myTest() {
-        String source = "\\Device\\NPF_{53152A2F-39F7-458E-BD58-24D17099256A}";
-        Pointer lpAdapter = mappings.PacketOpenAdapter(source);
-        NativeMappings.Structures.PPACKET_OID_DATA oid_data = new NativeMappings.Structures.PPACKET_OID_DATA(runtime, 6);
-        oid_data.Oid.set(0x01010102L);
-        oid_data.Length.set(6L);
-        printMacAddress(oid_data.Data);
-        mappings.PacketRequest(lpAdapter, false, oid_data);
-        printMacAddress(oid_data.Data);
-        mappings.PacketCloseAdapter(lpAdapter);
-    }
 
     @Test
     public void test01_PacketGetVersion() {
