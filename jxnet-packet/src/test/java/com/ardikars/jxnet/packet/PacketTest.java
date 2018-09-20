95afa32d50cdb00901790e3a8d07d572cf86f465
Macintosh <macintosh@Macintoshs.local>
2018-09-20 20:14:57 +0700

    Update gradle version

--- jxnet-packet/src/test/java/com/ardikars/jxnet/packet/PacketTest.java
+++ jxnet-packet/src/test/java/com/ardikars/jxnet/packet/PacketTest.java
@@ -1,6 +1,8 @@
 package com.ardikars.jxnet.packet;
 
+import com.ardikars.common.util.Platforms;
 import jnr.ffi.LibraryLoader;
+import jnr.ffi.Platform;
 import jnr.ffi.Runtime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,12 +15,18 @@ public class PacketTest {
 
     @Test
     public void test01_PacketGetVersion() {
-        System.out.println(mappings.PacketGetVersion());
+        if (Platforms.isWindows()) {
+            System.out.println(mappings.PacketGetVersion());
+        }
+        assert true;
     }
 
     @Test
     public void test02_PacketGetDriverVersion() {
-        System.out.println(mappings.PacketGetDriverVersion());
+        if (Platforms.isWindows()) {
+            System.out.println(mappings.PacketGetDriverVersion());
+        }
+        assert true;
     }
 
     @Test
