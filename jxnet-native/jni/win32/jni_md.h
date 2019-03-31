cc0301fc3a1d2eef6eba038826897e108f34b834
Ardika Rommy Sanjaya <ardikars@gmail.com>
2019-04-01 00:18:06 +0800

    Handle vs type

--- jxnet-native/jni/win32/jni_md.h
+++ jxnet-native/jni/win32/jni_md.h
@@ -26,6 +26,8 @@
 #ifndef _JAVASOFT_JNI_MD_H_
 #define _JAVASOFT_JNI_MD_H_
 
+#define __int64 long long
+
 #define JNIEXPORT __declspec(dllexport)
 #define JNIIMPORT __declspec(dllimport)
 #define JNICALL __stdcall
